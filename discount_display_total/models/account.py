c3392ee3829476909d08f34ed3ccee970d5ce361
Mas Ipin <mas.ipin@hashmicro.com>
2023-08-17 19:54:02 +0700

    [ADD] subtotal before disc

--- discount_display_total/models/account.py
+++ discount_display_total/models/account.py
@@ -5,11 +5,17 @@ class accountMove(models.Model):
     _inherit = 'account.move'
 
     total_discount = fields.Monetary('Total Discount', compute='get_total_discount_line', store=True)
+    undiscounted_total = fields.Monetary('Subtotal', compute='get_total_discount_line', store=True)
 
-    @api.depends('line_ids', 'line_ids.discount', 'line_ids.quantity')
+    @api.depends('line_ids', 'line_ids.discount', 'line_ids.quantity', 'tax_totals')
     def get_total_discount_line(self):
         for rec in self:
             rec.total_discount = sum([l.quantity*l.price_unit*l.discount/100 \
                 for l in rec.line_ids\
                     .filtered(lambda l: l.display_type == 'product')])
+            if rec.tax_totals:
+                rec.undiscounted_total = rec.tax_totals.get('amount_untaxed', 0.0) + rec.total_discount
+            else:
+                rec.undiscounted_total = sum([l.quantity*l.price_unit for l in rec.line_ids\
+                    .filtered(lambda ll: ll.display_type == 'product')])
     
--- discount_display_total/models/sale.py
+++ discount_display_total/models/sale.py
@@ -7,12 +7,13 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     total_discount = fields.Monetary('Total Discount', compute='get_total_discount_line', store=True)
+    undiscounted_total = fields.Monetary('Subtotal', compute='get_total_discount_line', store=True)
 
     @api.depends('order_line', 'order_line.discount')
     def get_total_discount_line(self):
         for rec in self:
             rec.total_discount = sum([l.product_uom_qty*l.price_unit*l.discount/100 for l in rec.order_line])
-    
-
-            
-
+            if rec.tax_totals:
+                rec.undiscounted_total = rec.tax_totals.get('amount_untaxed', 0.0) + rec.total_discount
+            else:
+                rec.undiscounted_total = sum([l.product_uom_qty*l.price_unit for l in rec.order_line])
