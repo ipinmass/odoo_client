7439d139c225563031aa62918800941e98ff05da
Mas Ipin <mas.ipin@hashmicro.com>
2023-09-09 22:20:56 +0700

    [ADD] swiss sale view

---
+++ swiss_sale/__manifest__.py
@@ -0,0 +1,28 @@
+
+{
+    "name": "Swiss Sales Modifier",
+    "version": "16.0.0",
+    "summary": """
+    
+""",
+    "author": "Ipin",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "website": "",
+    "live_test_url": "",
+    "depends": [
+            "base",
+            "sale",
+        ],
+    "data": [
+       'views/sale_view.xml',
+
+    ],
+    "images": [],
+    "assets": {
+        
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ swiss_sale/models/sale.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    nota_no = fields.Char('Nota No.')
+    order_type = fields.Selection([
+        ('shop', _('Soop')),
+        ('special', _('Special Order'))
+    ], string='Order Type')
+    delivery_type = fields.Selection([
+        ('self', _('Self-collected')),
+        ('delivered', _('Delivered'))
+    ], string='Delivery Type')
+    description = fields.Text(_('Description'))
