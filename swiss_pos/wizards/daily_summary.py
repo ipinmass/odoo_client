6aa8bb98152647bc7a8f2b76ccf65e30e42f95c3
Ipin <adm.masipin@gmail.com>
2023-10-05 07:38:25 +0700

    [ADD] report default

--- swiss_pos/wizards/daily_summary.py
+++ swiss_pos/wizards/daily_summary.py
@@ -3,7 +3,15 @@ from odoo import models, fields, _, api
 class dailySummary(models.TransientModel):
     _name = 'store.daily.summary'
 
-    session_ids = fields.Many2many()
+    session_ids = fields.Many2many('pos.session', string='PoS Sessions')
+    payment_ids = fields.Many2many('account.payment', string='Sales Payments')
+
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(dailySummary, self).default_get(fields_list)
+        
+        return res
 
     def print_report(self):
         self.ensure_one()
